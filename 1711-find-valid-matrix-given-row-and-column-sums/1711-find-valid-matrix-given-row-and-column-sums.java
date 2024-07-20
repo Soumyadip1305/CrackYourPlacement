4f6b6681d4c7e1a676feb082cb7995f78bd9c929
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2024-07-20 10:02:23 +0530

    Time: 6 ms (76.89%), Space: 52.8 MB (72.91%) - LeetHub

--- 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.java
+++ 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
-        int numRows = rowSum.length;
-        int numCols = colSum.length;
-        int[][] matrix = new int[numRows][numCols];
+        int m = rowSum.length;
+        int n = colSum.length;
+        int[][] mat = new int[m][n];
 
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
-                int minVal = Math.min(rowSum[i], colSum[j]);// Find the minimum value that can be placed in matrix[i][j]
-                matrix[i][j] = minVal;
-                
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int minVal = Math.min(rowSum[i], colSum[j]);// Find the minimum value that can be placed in mat[i][j]
+                mat[i][j] = minVal;
+
                 rowSum[i] -= minVal;// Update the row and column sums
                 colSum[j] -= minVal;
             }
         }
 
-        return matrix;
+        return mat;
     }
 }
