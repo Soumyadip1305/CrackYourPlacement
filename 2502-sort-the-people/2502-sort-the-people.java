f7517408bf7787536b783e20df3fc2a973a03738
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2024-07-22 09:12:14 +0530

    Time: 8 ms (62.47%), Space: 45.3 MB (32.21%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.java
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -9,7 +9,18 @@ class Solution {
         }
     }
     public String[] sortPeople(String[] names, int[] heights) {
-        int n = names.length;
+        /*       String[] ans = new String[heights.length];
+        Map<Integer, String> map = new HashMap<>();
+        for (int i = 0; i < names.length; i++) {
+            map.put(heights[i], names[i]);
+        }
+        System.out.println(map);
+        Arrays.sort(heights);
+        for (int i = 0; i < heights.length; i++) {
+            ans[heights.length-i-1] = map.get(heights[i]);
+        }
+        */
+    /*    int n = names.length;
         Person[] people = new Person[n];
         for (int i = 0; i < n; i++) {
             people[i] = new Person(names[i], heights[i]);
@@ -18,6 +29,20 @@ class Solution {
         String[] ans = new String[n];
         for (int i = 0; i < n; i++) {
             ans[i] = people[i].name;
+        }
+
+        return ans;
+
+     */
+        int n = names.length;
+        Integer[] indices = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            indices[i] = i;
+        }
+        Arrays.sort(indices, (a, b) -> heights[b] - heights[a]);
+        String[] ans = new String[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = names[indices[i]];
         }
 
         return ans;
