d10861e324ecd86db0a16cf92e98c9efe82c23ca
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2024-07-25 09:48:19 +0530

    Time: 18 ms (86.59%), Space: 55.6 MB (67.65%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+
+        Arrays.sort(nums);
+       /*  PriorityQueue<Integer>pq=new PriorityQueue<>();
+        for (int it : nums) {
+            pq.add(it);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]= pq.poll();
+        }*/
+    return nums;
+}
+}
