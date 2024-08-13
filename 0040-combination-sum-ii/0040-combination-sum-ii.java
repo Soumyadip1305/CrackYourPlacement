6c563c078658cdf429a090c35d26b2fcb6d590f4
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2024-08-13 09:54:23 +0530

    Time: 3 ms (82.44%), Space: 43 MB (85.22%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,28 +1,38 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-       Set<List<Integer>>ans=new HashSet<>();
+      /* Set<List<Integer>>ans=new HashSet<>();
         Arrays.sort(candidates);
         solve(0,candidates,target,ans,new ArrayList<>());
-        return new ArrayList<>(ans);
+        return new ArrayList<>(ans);*/
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(candidates);
+        solve(0, candidates, target, ans, new ArrayList<>());
+        return ans;
     }
 
-    private static void solve(int ind, int[] candidates, int target, Set<List<Integer>> ans,List<Integer>list) {
-        if (ind==candidates.length){
-            if (target==0) {
-                ans.add(new ArrayList<>(list));
-            }
+    private static void solve(int ind, int[] candidates, int target, List<List<Integer>> ans, List<Integer> list) {
+        if (target == 0) {
+            ans.add(new ArrayList<>(list));
             return;
         }
-        // pick
-        if (target>=candidates[ind]){
-            list.add(candidates[ind]);
-            solve(ind+1,candidates,target-candidates[ind],ans,list);
+        for (int i = ind; i < candidates.length; i++) {
+            if (i > ind && candidates[i] == candidates[i - 1]) {
+                continue; // skip duplicates
+            }
+            if (candidates[i] > target) {
+                break; // no need to continue if the current candidate exceeds the target
+            }
+            list.add(candidates[i]);
+            solve(i + 1, candidates, target - candidates[i], ans, list);
             list.remove(list.size() - 1);
         }
-        while (ind + 1 < candidates.length && candidates[ind] == candidates[ind + 1]) {
-        ind++;
-    }
-        // not pick
-        solve(ind+1,candidates,target,ans,list);  
+//        // pick
+//        if (target >= candidates[ind]) {
+//            list.add(candidates[ind]);
+//            solve(ind + 1, candidates, target - candidates[ind], ans, list);
+//            list.remove(list.size() - 1);
+//        }
+//        // not pick
+//        solve(ind + 1, candidates, target, ans, list);
     }
 }
