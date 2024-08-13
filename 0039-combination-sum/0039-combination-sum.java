897a9f90784690af85a369bc6cb31a5dd81a6536
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2024-08-13 12:38:31 +0530

    Time: 2 ms (84.16%), Space: 44.5 MB (48.23%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,26 +1,23 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Set<List<Integer>> ans = new HashSet<>();
-        Arrays.sort(candidates);
+       List<List<Integer>> ans = new ArrayList<>();
         solve(0, candidates, target, ans, new ArrayList<>());
-        return new ArrayList<>(ans);
+        return ans;
     }
 
-    private static void solve(int ind, int[] candidates, int target, Set<List<Integer>> ans, List<Integer> list) {
-        if (ind==candidates.length) {
-            if (target == 0) {
-                ans.add(new ArrayList<>(list));
-            }
+    private static void solve(int ind, int[] candidates, int target, List<List<Integer>> ans, List<Integer> list) {
+        if (target == 0) {
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        if (ind == candidates.length || target < 0) {
             return;
         }
 
-        // pick
-        if (target >= candidates[ind]) {
-            list.add(candidates[ind]);
-            solve(ind , candidates, target - candidates[ind], ans, list);
-            list.remove(list.size() - 1);
-        }
-        // not pick
+        list.add(candidates[ind]);
+        solve(ind, candidates, target - candidates[ind], ans, list);
+        list.remove(list.size() - 1);
+
         solve(ind + 1, candidates, target, ans, list);
     }
 }
