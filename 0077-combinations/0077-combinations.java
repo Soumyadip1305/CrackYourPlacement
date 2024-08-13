77184339a9538606a1ecf9af165afb945a146fd7
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2024-08-13 12:50:11 +0530

    Time: 18 ms (77.87%), Space: 94.4 MB (72.38%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,26 +1,26 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> ans = new ArrayList<>();
-        int[]candidates=new int[n];
-        for (int i = 1; i <=n ; i++) {
-            candidates[i-1]=i;
-        }
-        solve(0, candidates, k, ans, new ArrayList<>());
+        // int[] candidates = new int[n];
+        // for (int i = 1; i <= n; i++) {
+        //     candidates[i - 1] = i;
+        // }
+        solve(1, n, k, ans, new ArrayList<>());
         return ans;
     }
 
-    private static void solve(int ind, int[] candidates, int len, List<List<Integer>> ans, List<Integer> list) {
+    private static void solve(int i, int n, int len, List<List<Integer>> ans, List<Integer> list) {
         if (len == 0) {
             ans.add(new ArrayList<>(list));
             return;
         }
-        if (ind == candidates.length || len < 0) {
+        if (i > n || len < 0) {
             return;
         }
-        list.add(candidates[ind]);
-        solve(ind+1, candidates, len -1, ans, list);
+        list.add(i);
+        solve(i + 1, n, len - 1, ans, list);
         list.remove(list.size() - 1);
 
-        solve(ind + 1, candidates, len, ans, list);
+        solve(i + 1, n, len, ans, list);
     }
 }
