77324f6d76e7262ab015e6b6c8e9281c09260617
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2024-08-20 23:37:00 +0530

    Time: 2 ms (100%), Space: 42.3 MB (75.97%) - LeetHub

--- 1240-stone-game-ii/1240-stone-game-ii.java
+++ 1240-stone-game-ii/1240-stone-game-ii.java
@@ -1,37 +1,35 @@
 class Solution {
+        int n ;int[] sufSum;int[][] dp;
     public int stoneGameII(int[] piles) {
-        int n = piles.length;
-        int[] sufSum = new int[n];
+        n = piles.length;
+        sufSum = new int[n];
         sufSum[n - 1] = piles[n - 1];
-        int[][] dp = new int[n][n + 1];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= n; j++) {
-                dp[i][j] = -1;
-            }
+        dp = new int[n][n + 1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
         for (int i = n - 2; i >= 0; i--) {
             sufSum[i] = piles[i] + sufSum[i + 1];
         }
-        return allPossibleTake(piles, sufSum, 0, 1, dp);
+        return allPossibleTake(0, 1);
     }
 
-    private int allPossibleTake(int[] piles, int[] suffixSum, int ind, int m, int[][] dp) {
-        int n = piles.length;
+    private int allPossibleTake( int ind, int m) {
         if (ind >= n) {  // all piles have been taken
             return 0;
         }
         if (dp[ind][m] != -1) return dp[ind][m];
         // If Alice can take all the remaining piles
         if (ind + 2 * m >= n) {
-            return dp[ind][m]=suffixSum[ind];
+            return dp[ind][m]=sufSum[ind];
         }
 
         int maxi = 0;
 
         // Try taking all possible number of piles from 1 to 2*m
         for (int x = 1; x <= 2 * m; x++) {
-            int bob = allPossibleTake(piles, suffixSum, ind + x, Math.max(m, x), dp);
-            maxi = Math.max(maxi, suffixSum[ind] - bob);
+            int bob = allPossibleTake(ind + x, Math.max(m, x));
+            maxi = Math.max(maxi, sufSum[ind] - bob);
         }
 
         return dp[ind][m] = maxi;
