d44330516b86c7764fd2f5f9b1eca93317a2c52b
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2024-10-02 12:33:39 +0530

    Time: 74 ms (5.54%), Space: 62 MB (58.76%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -1,19 +1,21 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        if(arr.length==0)return arr;
-        int[] sortArr = arr.clone();
-        Arrays.sort(sortArr);
+        int n = arr.length;
+        if (n == 0) return arr;
+//        int[] sortArr = arr.clone();
+//        Arrays.sort(sortArr);
         int rank=1;
-        Map<Integer,Integer>map=new HashMap<>();
-        map.put(sortArr[0],rank);
-        for (int i = 1; i < arr.length; i++) {
-            if (sortArr[i-1]<sortArr[i]){
-                rank++;
-            }
-            map.put(sortArr[i],rank);
+//        Map<Integer,Integer>map=new HashMap<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int it : arr) {
+            map.put(it, 0);
         }
-        for (int i = 0; i < arr.length; i++) {
-            arr[i]=map.get(arr[i]);
+//        map.put(sortArr[0],rank);
+        for (int it:map.keySet()) {
+            map.put(it, rank++);
+        }
+        for (int i = 0; i < n; i++) {
+            arr[i] = map.get(arr[i]);
         }
         //System.out.println(map);
         return arr;
