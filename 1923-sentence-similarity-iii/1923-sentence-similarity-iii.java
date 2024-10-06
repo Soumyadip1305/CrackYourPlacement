c2379b360a2dc28ee77832c8ec540528de44b918
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2024-10-06 21:58:05 +0530

    Time: 1 ms (96.43%), Space: 41.9 MB (13.89%) - LeetHub

---
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        String[] s1 = sentence1.split(" ");
+        String[] s2 = sentence2.split(" ");
+
+        if (s1.length > s2.length) {
+            return areSentencesSimilar(sentence2, sentence1);
+        }
+
+        int i = 0;
+        int j = 0;
+
+        while (i < s1.length && s1[i].equals(s2[i])) {
+            i++;
+        }
+
+        while (j < s1.length - i && s1[s1.length - 1 - j].equals(s2[s2.length - 1 - j])) {
+            j++;
+        }
+
+        return i + j == s1.length;
+    }
+}
