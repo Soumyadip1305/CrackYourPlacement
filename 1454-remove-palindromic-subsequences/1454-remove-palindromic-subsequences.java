0f0ade0c250b9fb2ddb79b30f202ed22f1b3a2ca
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2024-12-03 23:25:39 +0530

    Time: 0 ms (100%), Space: 41.9 MB (5.34%) - LeetHub

---
+++ 1454-remove-palindromic-subsequences/1454-remove-palindromic-subsequences.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int removePalindromeSub(String s) {
+        if (isPalindrome(s)) {
+            return 1;
+        }
+        return 2;
+    }
+    private boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    /*
+       b a a b b
+       b b a a b
+
+     */
+}
