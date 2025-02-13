fc644208b6c4cfc07f9930202991aef2698121e7
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2025-02-13 13:05:34 +0530

    Time: 161 ms (7.41%), Space: 70.3 MB (88.48%) - LeetHub

---
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        /*
+            2,11,10,1,3
+            1 2 3 10 11
+            4 3 10 11
+
+   min(x, y) * 2 + max(x, y) =
+     */
+        int cnt=0;
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        for (int num : nums) {
+            pq.add((long) num);
+        }
+        while (pq.size() >= 2 && pq.peek() < k) {
+            long x = pq.poll();
+            long y = pq.poll();
+            pq.add(Math.min(x, y) * 2L + Math.max(x, y));
+            cnt++;
+        }
+        return cnt;        
+    }
+}
