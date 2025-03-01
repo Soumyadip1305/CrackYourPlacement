7cfb355870093f645a13762d724ada29113c4f5d
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2025-03-01 16:05:07 +0530

    Time: 1 ms (96.37%), Space: 43.6 MB (70.32%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -1,7 +1,5 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
-        int[] ans = new int[nums.length];
-        Arrays.fill(ans, 0);
         for (int i = 1; i < nums.length; i++) {
             if (nums[i - 1] == nums[i]) {
                 nums[i - 1] *= 2;
@@ -11,10 +9,12 @@ class Solution {
         int ind = 0;
         for (int it : nums) {
             if (it != 0) {
-                ans[ind++] = it;
+                nums[ind++] = it;
             }
         }
-        //System.out.println(Arrays.toString(nums));
-        return ans;
+        while (ind < nums.length) {
+            nums[ind++] = 0;
+        }
+        return nums;
     }
 }
