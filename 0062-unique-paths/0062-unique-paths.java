39dc98bad71f346983c32679253d6545d10149d2
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2025-03-07 09:06:01 +0530

    Time: 0 ms (100%), Space: 40.7 MB (25.58%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,25 +1,22 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        // (0,0)--->(m,n)
-        int[][]dp=new int[m+1][n+1];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                dp[i][j]=-1;
-            }
+      int[][]dp=new int[m][n];
+        for (int[]row:dp){
+            Arrays.fill(row,-1);
         }
-        return solve(m - 1, n - 1,dp);
+        return f(m - 1, n - 1,dp);
     }
 
-    private int solve(int r, int c, int[][] dp) {
-        if (r == 0 && c == 0) dp[0][0]= 1;
+    static int f(int r, int c, int[][] dp) {
+        if (r == 0 && c == 0) {
+            return dp[r][c]=1;
+        }
+        if (r < 0 || c < 0) {
+            return 0;
+        }
         if (dp[r][c]!=-1)return dp[r][c];
-        int cnt=0;
-        if (r>0){
-            cnt+=solve(r-1,c, dp);
-        }
-        if (c>0){
-            cnt+=solve(r,c-1, dp);
-        }
-        return dp[r][c]=cnt;
+        int down = f(r - 1, c, dp);
+        int right = f(r, c - 1, dp);
+        return dp[r][c]=down + right;  
     }
 }
