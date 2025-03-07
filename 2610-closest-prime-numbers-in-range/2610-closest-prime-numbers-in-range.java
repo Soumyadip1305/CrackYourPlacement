36d2467583399ef48af437fe08f832ac2a6f83ae
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2025-03-07 11:51:47 +0530

    Time: 95 ms (63.2%), Space: 50.4 MB (32%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
@@ -1,36 +1,39 @@
 class Solution {
     public int[] closestPrimes(int left, int right) {
-       // List<Integer> list = new ArrayList<>();
-        int prev = -1, curr = -1;
-        int[] ans = new int[2];
-        Arrays.fill(ans, -1);
-        int prevDis = Integer.MAX_VALUE;
+       boolean[] isPrime = sieve(right);
+        List<Integer> primes = new ArrayList<>();
         for (int i = left; i <= right; i++) {
-            if (isPrime(i)) {
-                if (prev == -1) {
-                    prev = i;
-                } else {
-                    curr = i;
-                    int currDis = curr - prev;
-                    if (currDis < prevDis) {
-                        prevDis = currDis;
-                        ans[0] = prev;
-                        ans[1] = curr;
-                    }
-                    prev = curr;
-                }
+            if (isPrime[i]) {
+                primes.add(i);
             }
         }
-        //System.out.println(list);
+        //System.out.println(primes);
+        int[] ans = {-1, -1};
+        int minDiff = Integer.MAX_VALUE;
+
+        for (int i = 1; i < primes.size(); i++) {
+            int diff = primes.get(i) - primes.get(i - 1);
+            if (diff < minDiff) {
+                minDiff = diff;
+                ans[0] = primes.get(i - 1);
+                ans[1] = primes.get(i);
+            }
+        }
         return ans;
     }
 
-    static boolean isPrime(int n) {
-        if (n < 2) return false;
+    private boolean[] sieve(int n) {
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
 
         for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) return false;
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
         }
-        return true;
+        return isPrime;
     }
 }
