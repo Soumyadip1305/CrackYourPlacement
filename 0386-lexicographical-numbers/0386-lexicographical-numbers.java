ed37338bbb2892e1310faf5db5b1877482e9e77b
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2025-06-09 23:45:22 +0530

    Time: 105 ms (5.07%), Space: 46.8 MB (99.81%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,28 +1,21 @@
 class Solution {
     public List<Integer> lexicalOrder(int n) {
-         List<Integer> res = new ArrayList<>();
+        List<Integer>list=new ArrayList<>();
 
-       String s = String.valueOf(n);
-       int len = s.length();
-
-       for(int i = 1; i <=9; i++ ){
-           add(res,String.valueOf(i),n);
-       } 
-
-       return res;
-
-    }
-
-    void add(List<Integer> res, String s,int n){
-     
-         int cur = Integer.parseInt(s);
-
-         if(cur > n) return ;
-
-         res.add(cur);
-
-        for(int i = 0; i <= 9; i++){
-            add(res, s + String.valueOf(i),n);
+        for(int i = 1; i <=9; i++ ){
+            add(list,String.valueOf(i),n);
         }
+
+        return list;
     }
+
+    private void add(List<Integer> list, String s, int n) {
+        int cur = Integer.parseInt(s);
+        if(cur > n) return ;
+        list.add(cur);
+
+        for (int i = 0; i <=9; i++) {
+            add(list,s+String.valueOf(i),n);
+    }
+}
 }
