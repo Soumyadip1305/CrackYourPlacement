ec5f5a17b16f0c05a4fd4f7870177e7fa4ded69e
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2026-01-15 12:22:23 +0530

    Time: 3 ms (100%), Space: 46.5 MB (10.29%) - LeetHub

--- 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.java
+++ 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
-        Arrays.sort(hBars);
-        Arrays.sort(vBars);
+       // Arrays.sort(hBars);
+       // Arrays.sort(vBars);
         int[]hxHy=longestConsecutive(hBars);
         int[]vxVy=longestConsecutive(vBars);
 
