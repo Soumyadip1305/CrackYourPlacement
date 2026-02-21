488ce20c67c82bf19ab9128ae74dd4d27a0af461
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2026-02-21 19:51:28 +0530

    Time: 25 ms (32.48%), Space: 47.1 MB (26.1%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,21 +1,34 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int[] freq = new int[26];
-        int i = 0, maxFreq = 0, ans = 0;
-
+        int i = 0, maxFre = 0, maxi = 0;
+        Map<Character, Integer> map = new HashMap<>();
         for (int j = 0; j < s.length(); j++) {
             char ch=s.charAt(j);
-            freq[ch - 'A']++;
-            maxFreq = Math.max(maxFreq, freq[ch - 'A']);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            maxFre = Math.max(map.get(ch), maxFre);
 
-            while ((j - i + 1) - maxFreq > k) {
-                freq[s.charAt(i) - 'A']--;
+            while ((j - i + 1) - maxFre > k) {
+                // means it's having mre than k difference need remove ith
+                char lch = s.charAt(i);
+               // if (map.get(lch) == maxFre) maxFre--;
+                map.put(lch, map.get(lch) - 1);
                 i++;
             }
-
-            ans = Math.max(ans, j - i + 1);
+            maxi=Math.max(maxi,j-i+1);
         }
-
-        return ans;
+        return maxi;
     }
+    /*
+one problem occurs that when I remove B there max freq=B=A, so in this case it remove A also
+    s =
+"BAAA"
+k =
+0
+
+Use Testcase
+Output
+2
+Expected
+3
+*/
 }
