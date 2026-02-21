7a0758bef28b46835e10c3a7418b5ca3c13c67e3
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2026-02-21 19:57:51 +0530

    Time: 6 ms (98.76%), Space: 46.4 MB (50.93%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,34 +1,22 @@
 class Solution {
     public int characterReplacement(String s, int k) {
+       int[] arr = new int[26];
         int i = 0, maxFre = 0, maxi = 0;
-        Map<Character, Integer> map = new HashMap<>();
+
         for (int j = 0; j < s.length(); j++) {
-            char ch=s.charAt(j);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-            maxFre = Math.max(map.get(ch), maxFre);
+
+            int ch=s.charAt(j)-'A';
+            arr[ch]++;
+            maxFre = Math.max(maxFre, arr[ch]);
 
             while ((j - i + 1) - maxFre > k) {
-                // means it's having mre than k difference need remove ith
-                char lch = s.charAt(i);
-               // if (map.get(lch) == maxFre) maxFre--;
-                map.put(lch, map.get(lch) - 1);
+                arr[s.charAt(i)-'A']--;
                 i++;
             }
-            maxi=Math.max(maxi,j-i+1);
+
+            maxi = Math.max(maxi, j - i + 1);
         }
+
         return maxi;
     }
-    /*
-one problem occurs that when I remove B there max freq=B=A, so in this case it remove A also
-    s =
-"BAAA"
-k =
-0
-
-Use Testcase
-Output
-2
-Expected
-3
-*/
 }
