cf9471f8b307b99327de5b997f7633ebb4513e6e
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2026-05-08 21:31:18 +0530

    Time: 15 ms (24.15%), Space: 46.6 MB (15.28%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character,Integer>map=new HashMap<>();
+        if(s.length()!=t.length()){
+            return false;
+        }
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(char ch:t.toCharArray()){
+            if(map.containsKey(ch)&&map.get(ch)>0){
+                map.put(ch,map.get(ch)-1);
+            }else{
+                return false ;
+            }
+        }
+        return true;
+    }
+}
