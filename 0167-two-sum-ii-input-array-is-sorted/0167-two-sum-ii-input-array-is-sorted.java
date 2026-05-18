616f45df514af5b748502288716ec79fa0f8c4c2
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2026-05-18 21:57:49 +0530

    Time: 4 ms (15.42%), Space: 48.4 MB (59.77%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,21 +1,30 @@
 class Solution {
-    public int[] twoSum(int[] num, int tar) {
-        int l=0,r=num.length-1;
-        while(l<r){
-            int sum=num[l]+num[r];
-            /*if(nums[l]==nums[r]){
-                l++;
-            } no need
-            */
-            if(sum==tar){
-                return new int[]{l+1,r+1};
-            }
-            else if(sum>tar){
-                r--;
-            }else{
-                l++;
+    public int[] twoSum(int[] numbers, int target) {
+        for (int i=0;i<numbers.length;i++){
+            int ind=searchInd(numbers,i+1,target-numbers[i]);
+            if (ind!=-1){
+                return new int[]{i+1,ind+1};
             }
         }
-        return new int[]{l+1,r+1};
+        return new int[]{-1,-1};
+    }
+
+
+    static int searchInd(int[]nums,int l,int tar){
+        int r = nums.length - 1;
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+
+            if (nums[mid] == tar) {
+                return mid;
+            } else if (nums[mid] < tar) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return -1;
     }
 }
